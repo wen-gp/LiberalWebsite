29121f208aadd6dacf1804eed4acef934c36c38a
wgp-code <839427924@qq.com>
2024-03-28 11:53:48 +0800

    fix：修改header

--- JS/Header.js
+++ JS/Header.js
@@ -4,13 +4,14 @@ function initHeaders() {
     header.id = "header";
     header.style.width = "100%";
     header.style.height = "70px";
-    header.style.background = "rgba(255, 255, 255, 0)";
+    header.style.background = "rgba(0, 0, 0, 0.3)";
     header.style.position = "fixed";
     header.style.top = 0;
     header.style.left = 0;
     header.style.transition = "box-shadow 0.3s, height 0.3s, background 0.3s";
     header.classList.add("scrolled");
     header.style.zIndex = 100;
+    document.body.appendChild(header);
 
     let logo = document.createElement('img');
     logo.src = '../Images/logo2.png';
@@ -19,35 +20,45 @@ function initHeaders() {
     logo.style.position = "absolute";
     logo.style.left = "8%";
     logo.style.top = "15%";
+    logo.style.transition = "height 0.3s";
     header.appendChild(logo);
 
-    //生成Home
-    let home = createHeaderElement("Home", "230px");
-    header.appendChild(home);
-
-    document.body.appendChild(header);
+    let elements = [];
+    createHeaderElement(elements, header, "Home", "230px", "./Home.html");
+    createHeaderElement(elements, header, "About", "330px", "./About.html");
+    createHeaderElement(elements, header, "Content", "430px", "./Content.html");
 
     window.addEventListener("scroll", function () {
         if (window.scrollY > 0) {
             header.style.height = "50px";
             header.style.background = "rgba(255, 255, 255, 1)";
             header.style.boxShadow = "0 5px 5px rgba(0, 0, 0, 0.3)";
+            logo.height = 40;
 
-            home.style.top = "15px";
-            home.style.color = "black"
+            for(let i = 0; i < elements.length; i++)
+            {
+                elements[i].style.top = "15px";
+                elements[i].style.color = "black"
+            }
         } else {
             header.style.height = "70px";
             header.style.background = "rgba(0, 0, 0, 0.3)";
             header.style.boxShadow = "";
+            logo.height = 50;
 
-            home.style.top = "25px";
-            home.style.color = "white"
+            for(let i = 0; i < elements.length; i++)
+            {
+                elements[i].style.top = "25px";
+                elements[i].style.color = "white"
+            }
         }
     });
 }
-function createHeaderElement(text, left) {
-    let element = document.createElement('div');
+function createHeaderElement(elements, header, text, left, jump) {
+    let element = document.createElement('a');
     element.innerText = text;
+    element.style.textDecoration = "none";
+    element.href = jump
     element.style.color = "white";
     element.style.transition = "top 0.3s, color 0.3s";
     element.style.cursor = "pointer";
@@ -65,5 +76,7 @@ function createHeaderElement(text, left) {
             this.style.color = "white"
         }
     });
+    header.appendChild(element);
+    elements.push(element);
     return element;
 }
