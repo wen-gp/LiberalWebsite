bd4cd6b881e9cb7255f6ecc73aa3b16812990bff
wgp-code <839427924@qq.com>
2024-04-10 15:36:40 +0800

    fix：优化

--- JS/Products.js
+++ JS/Products.js
@@ -117,47 +117,47 @@ class ProductBuilder {
     #boxCreator = new BoxCreator();
     #productCards = [
         {
-            type: "test1",
-            card: new ProductCard("../Images/产品模块1.webp", "iGS412", "No shutter infrared movement", "382×288", "12μm", "<30mK"),
-            product: {
-                name:"GST212W",
-                remark: "Wafer-Level-Package 256×192/12μm VOx Microbolometer Uncooled Thermal Detector Sensor",
-                description:
-                "GST212W is one of the Wafer Level Packing (WLP) uncooled infrared detectors developed " +
-                "by Global Sensor Technology (GST). Wafer level packaging (WLP) is the process of completing " +
-                "high vacuum packaging directly on the entire MEMS wafer, then scribing and cutting to make " +
-                "a single infrared detector.\n\nThis long wave vanadium oxide (VOx) microbolometer focal plane " +
-                "array IR detector covers wavelength from 8 to 14μm. And its resolution is 256×192 with pixel " +
-                "size at 12μm.\n\nThe GST212W infrared FPA detector is specially developed for small size, light " +
-                "weight & low price (SWaP), which has achieved high volume production with annual output up to " +
-                "1,000,000 pieces. Due to its lightweight and low price, GST212W uncooled thermal detector can " +
-                "be widely used in many industries such as Thermography, Security Monitoring, Outdoors, AIoT, " +
-                "Intelligent Hardware, Smart Building, Smart Home, UAV Payloads, Epidemic Prevention, ADAS and " +
-                "many other new emerging markets like consumer electronics. \n\nUntil now, we have provided our " +
-                "customers with various mature and stable infrared thermal imaging solutions. It’s very easy for " +
-                "WLP ir detector sensors developed by GST to be integrated into infrared thermal camera products " +
-                "and greatly reduces the cost for customers.",
-                features:[
-                    { title: "Meet Growing Demands", items: ["Volume production at lower cost"] },
-                    { title: "Easier Integration", items: ["Ultra-small size", "Ultra-light weight", "TECless technology"] },
-                    { title: "Optimal & Sharp Imaging", items: ["High sensitivity, typical NETD < 40mK", "Excellent image quality with frame rate at 30Hz"] }
+            "type": "test1",
+            "card": { "imgPath": "../Images/产品模块1.webp", "name": "iGS412", "description": "No shutter infrared movement", "resolution": "382×288", "pixelSize": "12μm", "netd": "<30mK" },
+            "product": {
+                "name": "GST212W",
+                "remark": "Wafer-Level-Package 256×192/12μm VOx Microbolometer Uncooled Thermal Detector Sensor",
+                "description":
+                    "GST212W is one of the Wafer Level Packing (WLP) uncooled infrared detectors developed " +
+                    "by Global Sensor Technology (GST). Wafer level packaging (WLP) is the process of completing " +
+                    "high vacuum packaging directly on the entire MEMS wafer, then scribing and cutting to make " +
+                    "a single infrared detector.\n\nThis long wave vanadium oxide (VOx) microbolometer focal plane " +
+                    "array IR detector covers wavelength from 8 to 14μm. And its resolution is 256×192 with pixel " +
+                    "size at 12μm.\n\nThe GST212W infrared FPA detector is specially developed for small size, light " +
+                    "weight & low price (SWaP), which has achieved high volume production with annual output up to " +
+                    "1,000,000 pieces. Due to its lightweight and low price, GST212W uncooled thermal detector can " +
+                    "be widely used in many industries such as Thermography, Security Monitoring, Outdoors, AIoT, " +
+                    "Intelligent Hardware, Smart Building, Smart Home, UAV Payloads, Epidemic Prevention, ADAS and " +
+                    "many other new emerging markets like consumer electronics. \n\nUntil now, we have provided our " +
+                    "customers with various mature and stable infrared thermal imaging solutions. It’s very easy for " +
+                    "WLP ir detector sensors developed by GST to be integrated into infrared thermal camera products " +
+                    "and greatly reduces the cost for customers.",
+                "features": [
+                    { "title": "Meet Growing Demands", "items": ["Volume production at lower cost"] },
+                    { "title": "Easier Integration", "items": ["Ultra-small size", "Ultra-light weight", "TECless technology"] },
+                    { "title": "Optimal & Sharp Imaging", "items": ["High sensitivity, typical NETD < 40mK", "Excellent image quality with frame rate at 30Hz"] }
                 ],
-                parameters:[
-                    { name: "Model", value: "GST212W" },
-                    { name: "Sensitive Material", value: "Vanadium Oxide" },
-                    { name: "Resolution", value: "256×192" },
-                    { name: "Pixel Size", value: "12μm" },
-                    { name: "Spectral Range", value: "8~14μm" },
-                    { name: "Typical NETD", value: "<40mK" },
-                    { name: "Digital Output", value: "Built-in 14 bits ADC" },
-                    { name: "Thermal Time Constant", value: "<12ms" },
-                    { name: "Max Frame Rate", value: "30Hz" },
-                    { name: "Power Consumption", value: "≤75mW" },
-                    { name: "Size (mm)", value: "10.53×7.44×1.45 (Without PCB Board)" },
-                    { name: "Weight", value: "<0.5g" },
-                    { name: "Operating Temperature", value: "-40°C ~ +85°C" }
+                "parameters": [
+                    { "name": "Model", "value": "GST212W" },
+                    { "name": "Sensitive Material", "value": "Vanadium Oxide" },
+                    { "name": "Resolution", "value": "256×192" },
+                    { "name": "Pixel Size", "value": "12μm" },
+                    { "name": "Spectral Range", "value": "8~14μm" },
+                    { "name": "Typical NETD", "value": "<40mK" },
+                    { "name": "Digital Output", "value": "Built-in 14 bits ADC" },
+                    { "name": "Thermal Time Constant", "value": "<12ms" },
+                    { "name": "Max Frame Rate", "value": "30Hz" },
+                    { "name": "Power Consumption", "value": "≤75mW" },
+                    { "name": "Size (mm)", "value": "10.53×7.44×1.45 (Without PCB Board)" },
+                    { "name": "Weight", "value": "<0.5g" },
+                    { "name": "Operating Temperature", "value": "-40°C ~ +85°C" }
                 ],
-                carouselChartImgPaths:["../Images/产品模块1.webp", "../Images/产品模块2.webp", "../Images/产品模块3.webp"],
+                "carouselChartImgPaths": ["../Images/产品模块1.webp", "../Images/产品模块2.webp", "../Images/产品模块3.webp"],
             }
         },
     ];
@@ -165,8 +165,8 @@ class ProductBuilder {
         let n = 0;
         let box;
         let rowCount = Math.floor(this.#productCards.length / 4);
-        if (this.#productCards.length % 4 > 0) { 
-            rowCount++; 
+        if (this.#productCards.length % 4 > 0) {
+            rowCount++;
         }
         for (let i = 0; i < this.#productCards.length; i++) {
             if (n == 0) {
@@ -177,7 +177,9 @@ class ProductBuilder {
             }
             const productCard = this.#productCards[i];
             if (productCard.type == this.#getQueryParam('productType')) {
-                productCard.card.create(box, productCard.product);
+                let c = productCard.card;
+                let card = new ProductCard(c.imgPath, c.name, c.description, c.resolution, c.pixelSize, c.netd);
+                card.create(box, productCard.product);
             }
             n++;
             if (n == 4) {
